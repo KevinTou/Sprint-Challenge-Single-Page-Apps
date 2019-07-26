bea15874dd67e100ef1874a40ddd7a92e5eaf60e
Kevin Tou <toukevin@gmail.com>
2019-07-26 11:04:58 -0700

    Updated character page with pagination

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -10,10 +10,10 @@ export default function AppRouter() {
   return (
     <div className="page-view ui bottom attached segment active tab">
       <Switch>
+        <Route exact path="/" component={WelcomePage} />
         <Route path="/characters" component={CharacterList} />
         <Route path="/locations" component={LocationsList} />
         <Route path="/episodes" component={EpisodesList} />
-        <Route component={WelcomePage} />
       </Switch>
     </div>
   );
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Pagination } from "semantic-ui-react";
 
 import SearchForm from "./SearchForm";
 import CharacterCard from "./CharacterCard";
@@ -8,10 +9,16 @@ export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [charData, setCharData] = useState(null);
   const [name, setName] = useState(null);
+  const [page, setPage] = useState(null);
+  const [count, setCount] = useState(null);
 
   const onSearch = (event, query) => {
     event.preventDefault();
     setName(query.name);
+  };
+
+  const getCurrentPage = page => {
+    setPage(page.activePage);
   };
 
   useEffect(() => {
@@ -20,22 +27,36 @@ export default function CharacterList() {
     const getCharData = async () => {
       return await axios.get(
         "https://rickandmortyapi.com/api/character/",
-        name && {
-          params: {
-            name: name
-          }
-        }
+        name
+          ? page
+            ? {
+                params: {
+                  name: name,
+                  page: page
+                }
+              }
+            : {
+                params: {
+                  name: name
+                }
+              }
+          : page && {
+              params: {
+                page: page
+              }
+            }
       );
     };
 
     getCharData()
       .then(res => {
         setCharData(res.data.results);
+        setCount(res.data.info.pages);
       })
       .catch(err => {
         console.log("Error: ", err);
       });
-  }, [name]);
+  }, [name, page]);
 
   if (!charData) {
     return <div>Loading...</div>;
@@ -43,12 +64,21 @@ export default function CharacterList() {
 
   return (
     <>
-      <SearchForm onSearch={onSearch} />
+      <SearchForm className="search" onSearch={onSearch} />
       <section className="character-list grid-view">
         {charData.map(char => {
           return <CharacterCard key={char.id} char={char} />;
         })}
       </section>
+      <div className="pagination-container">
+        {count && (
+          <Pagination
+            defaultActivePage={1}
+            totalPages={count}
+            onPageChange={(event, data) => getCurrentPage(data)}
+          />
+        )}
+      </div>
     </>
   );
 }
