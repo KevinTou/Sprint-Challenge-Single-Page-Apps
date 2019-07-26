be6d748487a2ce5cf41e42495ddf1032c48acec2
Kevin Tou <toukevin@gmail.com>
2019-07-26 11:18:06 -0700

    Added search name feature for episode and location

--- src/components/EpisodesList.js
+++ src/components/EpisodesList.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import SearchForm from "./SearchForm";
 import EpisodeCard from "./EpisodeCard";
 
 export default function EpisodesList() {
   // TODO: Add useState to track data from useEffect
   const [episodeData, setEpisodeData] = useState(null);
+  const [name, setName] = useState(null);
+
+  const onSearch = (event, query) => {
+    event.preventDefault();
+    setName(query.name);
+  };
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     const getEpisodeData = async () => {
-      return await axios.get("https://rickandmortyapi.com/api/episode/");
+      return await axios.get(
+        "https://rickandmortyapi.com/api/episode/",
+        name && {
+          params: {
+            name: name
+          }
+        }
+      );
     };
 
     getEpisodeData()
@@ -21,17 +35,20 @@ export default function EpisodesList() {
       .catch(err => {
         console.log("Error: ", err);
       });
-  }, []);
+  }, [name]);
 
   if (!episodeData) {
     return <div>Loading...</div>;
   }
 
   return (
-    <section className="episode-list grid-view">
-      {episodeData.map(episode => {
-        return <EpisodeCard key={episode.id} episode={episode} />;
-      })}
-    </section>
+    <>
+      <SearchForm className="search" onSearch={onSearch} />
+      <section className="episode-list grid-view">
+        {episodeData.map(episode => {
+          return <EpisodeCard key={episode.id} episode={episode} />;
+        })}
+      </section>
+    </>
   );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import SearchForm from "./SearchForm";
 import LocationCard from "./LocationCard";
 
 export default function LocationsList() {
   // TODO: Add useState to track data from useEffect
   const [locationData, setLocationData] = useState(null);
+  const [name, setName] = useState(null);
+
+  const onSearch = (event, query) => {
+    event.preventDefault();
+    setName(query.name);
+  };
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     const getLocationData = async () => {
-      return await axios.get("https://rickandmortyapi.com/api/location/");
+      return await axios.get(
+        "https://rickandmortyapi.com/api/location/",
+        name && {
+          params: {
+            name: name
+          }
+        }
+      );
     };
 
     getLocationData()
@@ -21,17 +35,20 @@ export default function LocationsList() {
       .catch(err => {
         console.log("Error: ", err);
       });
-  }, []);
+  }, [name]);
 
   if (!locationData) {
     return <div>Loading...</div>;
   }
 
   return (
-    <section className="location-list grid-view">
-      {locationData.map(location => {
-        return <LocationCard key={location.id} location={location} />;
-      })}
-    </section>
+    <>
+      <SearchForm className="search" onSearch={onSearch} />
+      <section className="location-list grid-view">
+        {locationData.map(location => {
+          return <LocationCard key={location.id} location={location} />;
+        })}
+      </section>
+    </>
   );
 }
