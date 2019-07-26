547d0f6d6ddd228ff4078321491b75a66c2a0a00
Kevin Tou <toukevin@gmail.com>
2019-07-26 09:27:45 -0700

    Updated Location page

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import LocationCard from "./LocationCard";
 
 export default function LocationsList() {
+  // TODO: Add useState to track data from useEffect
+  const [locationData, setLocationData] = useState(null);
 
+  useEffect(() => {
+    // TODO: Add AJAX/API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getLocationData = async () => {
+      return await axios.get("https://rickandmortyapi.com/api/location/");
+    };
+
+    getLocationData()
+      .then(res => {
+        console.log(res.data.results);
+        setLocationData(res.data.results);
+      })
+      .catch(err => {
+        console.log("Error: ", err);
+      });
+  }, []);
+
+  if (!locationData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section className="location-list grid-view">
+      {locationData.map(location => {
+        return <LocationCard key={location.name} location={location} />;
+      })}
+    </section>
+  );
 }
