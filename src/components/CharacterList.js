3895e40b308425b9f8daac5761f86a49bfeb7281
Kevin Tou <toukevin@gmail.com>
2019-07-26 10:13:51 -0700

    Updated SearchForm and incorporated into character

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import SearchForm from "./SearchForm";
 import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [charData, setCharData] = useState(null);
+  const [name, setName] = useState(null);
+
+  const onSearch = (event, query) => {
+    event.preventDefault();
+    setName(query.name);
+  };
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     const getCharData = async () => {
-      return await axios.get("https://rickandmortyapi.com/api/character/");
+      return await axios.get(
+        "https://rickandmortyapi.com/api/character/",
+        name && {
+          params: {
+            name: name
+          }
+        }
+      );
     };
 
     getCharData()
@@ -21,17 +35,20 @@ export default function CharacterList() {
       .catch(err => {
         console.log("Error: ", err);
       });
-  }, []);
+  }, [name]);
 
   if (!charData) {
     return <div>Loading...</div>;
   }
 
   return (
-    <section className="character-list grid-view">
-      {charData.map(char => {
-        return <CharacterCard key={char.id} char={char} />;
-      })}
-    </section>
+    <>
+      <SearchForm onSearch={onSearch} />
+      <section className="character-list grid-view">
+        {charData.map(char => {
+          return <CharacterCard key={char.id} char={char} />;
+        })}
+      </section>
+    </>
   );
 }
